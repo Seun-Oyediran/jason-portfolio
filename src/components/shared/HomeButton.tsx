c1af184d924995dfaf92f6f07749df76ea5743b1
Seun Oyediran <seunoyediran22517@gmail.com>
2022-12-02 03:20:06 +0100

    mobile home works

--- src/components/shared/HomeButton.tsx
+++ src/components/shared/HomeButton.tsx
@@ -5,12 +5,15 @@ interface IProps {
   text?: string;
   value?: number;
   active?: boolean;
+  className?: string;
 }
 
 const HomeButton = (props: IProps) => {
-  const { text, value, active } = props;
+  const {
+    text, value, active, className = '',
+  } = props;
   return (
-    <div className={`app_home_button_con ${active ? 'active' : ''} my-3`}>
+    <div className={`app_home_button_con ${active ? 'active' : ''} my-2 py-1 ${className}`}>
       <div className="d-flex align-items-end gap-1">
         <h2>{text}</h2>
         <RenderIf condition={!!value}>
